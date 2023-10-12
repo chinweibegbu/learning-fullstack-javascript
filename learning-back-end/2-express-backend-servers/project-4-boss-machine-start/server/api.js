df3a49d6b7ed4a3bd13c326b1c9ff954389d76a4
chinweibegbu <chinwe.ibegbu@gmail.com>
2023-10-12 15:37:33 +0100

    Added /ideas router endpoints

--- learning-back-end/2-express-backend-servers/project-4-boss-machine-start/server/api.js
+++ learning-back-end/2-express-backend-servers/project-4-boss-machine-start/server/api.js
@@ -20,6 +20,21 @@ const minionIdChecker = (req, res, next, id) => {
 };
 apiRouter.param('minionId', minionIdChecker);
 
+const IdeaIdChecker = (req, res, next, id) => {
+    const ideas = service.getAllFromDatabase('ideas');
+
+    const foundIndex = ideas.findIndex((idea) => {
+        return idea.id === id;
+    });
+    if (foundIndex !== -1) {
+        req.ideaId = id;
+        next();
+    } else {
+        res.status(404).send('Idea not found!');
+    }
+};
+apiRouter.param('ideaId', IdeaIdChecker);
+
 // MINION ENDPOINTS
 apiRouter.get('/minions', (req, res, next) => {
     const minions = service.getAllFromDatabase('minions');
@@ -56,13 +71,13 @@ apiRouter.put('/minions/:minionId', (req, res, next) => {
     try {
         // Add ID to details
         updatedMinionDetails["id"] = req.minionId;
-        
+
         // If the body does not includes salary, add the original minion's salary
         const originalMinionDetails = service.getFromDatabaseById('minions', req.minionId);
         if (!updatedMinionDetails.salary) {
             updatedMinionDetails["salary"] = originalMinionDetails.salary;
         }
-        
+
         // Update minion
         const updatedMinion = service.updateInstanceInDatabase('minions', updatedMinionDetails);
 
@@ -75,6 +90,78 @@ apiRouter.put('/minions/:minionId', (req, res, next) => {
 
 apiRouter.delete('/minions/:minionId', (req, res, next) => {
     const deleteIsSuccessful = service.deleteFromDatabasebyId('minions', req.minionId);
+
+    if (deleteIsSuccessful) {
+        res.status(204).send();
+    } else {
+        res.status(400).send("Delete operation failed");
+    }
+});
+
+// IDEA ENDPOINTS
+apiRouter.get('/ideas', (req, res, next) => {
+    const ideas = service.getAllFromDatabase('ideas');
+    res.send(ideas);
+});
+
+apiRouter.get('/ideas/:ideaId', (req, res, next) => {
+    const idea = service.getFromDatabaseById('ideas', req.ideaId);
+    res.send(idea);
+});
+
+apiRouter.post('/ideas', (req, res, next) => {
+    const ideaDetails = req.body;
+
+    if ('numWeeks' in ideaDetails && Number(ideaDetails.numWeeks) < 0) {
+        res.status(400).send("numWeeks cannot be negative");
+    }
+    if ('weeklyRevenue' in ideaDetails && Number(ideaDetails.weeklyRevenue) < 0) {
+        res.status(400).send("weeklyRevenue cannot be negative");
+    }
+
+    try {
+        const createdIdea = service.addToDatabase('ideas', ideaDetails);
+        res.status(201).send(createdIdea);
+    } catch (e) {
+        res.status(400).send(e.message);
+    }
+});
+
+apiRouter.put('/ideas/:ideaId', (req, res, next) => {
+    let updatedIdeaDetails = req.body;
+
+    if ('numWeeks' in updatedIdeaDetails && Number(updatedIdeaDetails.numWeeks) < 0) {
+        res.status(400).send("numWeeks cannot be negative");
+    }
+    if ('weeklyRevenue' in updatedIdeaDetails && Number(updatedIdeaDetails.weeklyRevenue) < 0) {
+        res.status(400).send("weeklyRevenue cannot be negative");
+    }
+
+    try {
+        // Add ID to details
+        updatedIdeaDetails["id"] = req.ideaId;
+
+        // Ensure all properties are complete before using the DB method
+        const originalIdeaDetails = service.getFromDatabaseById('ideas', req.ideaId);
+        const ideaPropertyNames = ["name", "description", "numWeeks", "weeklyRevenue"];
+        ideaPropertyNames.forEach((propertyName) => {
+            if (!updatedIdeaDetails[propertyName]) {
+                updatedIdeaDetails[propertyName] = originalIdeaDetails[propertyName];
+            }
+        });
+
+        // Update idea
+        const updatedIdea = service.updateInstanceInDatabase('ideas', updatedIdeaDetails);
+
+        // Send updated idea with a 200 OK status
+        res.send(updatedIdea);
+    } catch (e) {
+        res.status(400).send(e.message);
+    }
+});
+
+apiRouter.delete('/ideas/:ideaId', (req, res, next) => {
+    const deleteIsSuccessful = service.deleteFromDatabasebyId('ideas', req.ideaId);
 
     if (deleteIsSuccessful) {
         res.status(204).send();
